3c9192f1d861e484e7e767d33b6ba26b68afad0f
Sean Wong <seanwong24@live.com>
2024-05-27 20:18:55 -0700

    add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,44 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Choices",
+        short_name: "Choices",
+        start_url: "/",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "./icon.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "./splash.png",
+            sizes: "2732x2732",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+});
